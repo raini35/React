faea8da3bdd3e1fe5969737a1204a77b2e9a372c
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-21 19:24:54 -0500

    Created a web page without using webpack

---
+++ Webpack/unpacking-webpack/src/guestList.js
@@ -0,0 +1,27 @@
+var GuestList = [
+	{
+		"name": "Rainier Go", 
+		"phone": "832-475-6489", 
+		"email": "rainier.f.go@gmail.com"
+	}, 
+
+	{
+		"name": "John Doe", 
+		"phone": "111-222-3333", 
+		"email": "johndoe@aol.com"
+	}, 
+
+	{
+		"name": "Jane Smith", 
+		"phone": "444-555-6666", 
+		"email": "janesmith@hotmail.com"
+	},
+
+	{
+		"name": "Bob Miller", 
+		"phone": "777-888-9999", 
+		"email": "bobmiller@gmail.com"
+	}
+
+
+]; 
---
+++ Webpack/unpacking-webpack/src/showGuestNames.js
@@ -0,0 +1,14 @@
+var ShowGuestNames = function () {
+	var ul = document.createElement('ul'); 
+	GuestList.map(function(guest) {
+	//	var email = document.createTextNode(guest.email); 
+	//	var number = document.createTextNode(guest.phone); 
+		var name = document.createTextNode(guest.name); 
+		var li = document.createElement('li');  
+		li.appendChild(name); 
+	//	li.appendChild(number); 
+	//	li.appendChild(email); 
+		ul.appendChild(li); 
+	}); 
+	document.getElementById('list').appendChild(ul); 
+}; 
