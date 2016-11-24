4cfe89f6d259605c1368657553073d3b9db73b47
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-23 23:49:05 -0500

    Added access state in constructor

--- react-gif-search/src/index.js
+++ react-gif-search/src/index.js
@@ -11,6 +11,8 @@ class App extends React.Component {
     this.state = {
       gifs: []
     }
+    
+    this.handleTermChange = this.handleTermChange.bind(this); 
   }
 
   handleTermChange(term) {
