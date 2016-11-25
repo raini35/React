fd459d9a2e39e09f7467a0343f5d216b981eb04c
Rainier F. Go <rainier.f.go@gmail.com>
2016-11-25 15:19:52 -0500

    Finished part 1

--- react-gif-search-take-two/src/components/SearchBar.js
+++ react-gif-search-take-two/src/components/SearchBar.js
@@ -5,10 +5,16 @@ class SearchBar extends React.Component {
 		super(props); 
 		this.state = {term: ''}
 	}
+	
+	onInputChange(term) {
+		this.setState({term}); 
+		this.props.onTermChange(term); 
+	}
+	
 	render() {
 		return (
 			<div className="search"> 
-				<input onChange={event => this.onInputChange(event.target.value)} />
+       	 		<input placeholder="Enter text to search for gifs!" onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		); 
 	}
