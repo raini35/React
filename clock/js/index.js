c4b28504f984878545aeaa838ba9910051cddea0
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-08 21:46:45 -0500

    Added clock react folder

---
+++ clock/js/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function Clock(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Hello, world!'
+    ),
+    React.createElement(
+      'h2',
+      null,
+      'It is ',
+      props.date.toLocaleTimeString(),
+      '.'
+    )
+  );
+}
+
+function tick() {
+  ReactDOM.render(React.createElement(Clock, { date: new Date() }), document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
