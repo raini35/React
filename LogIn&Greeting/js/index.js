22245c22f0fe1ecafbda0714f2096364e2a14670
Rainier F. Go <rainier.f.go@gmail.com>
2016-12-10 20:00:21 -0500

    Added LogIn&Greeting React files

---
+++ LogIn&Greeting/js/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Toggle = function (_React$Component) {
+  _inherits(Toggle, _React$Component);
+
+  function Toggle(props) {
+    _classCallCheck(this, Toggle);
+
+    var _this = _possibleConstructorReturn(this, _React$Component.call(this, props));
+
+    _this.state = { isToggleOn: true };
+
+    _this.handleClick = _this.handleClick.bind(_this);
+    return _this;
+  }
+
+  Toggle.prototype.handleClick = function handleClick() {
+    this.setState(function (prevState) {
+      return {
+        isToggleOn: !prevState.isToggleOn
+      };
+    });
+  };
+
+  Toggle.prototype.render = function render() {
+    return React.createElement(
+      'button',
+      { onClick: this.handleClick },
+      this.state.isToggleOn ? 'HELLO' : "GOODBYE"
+    );
+  };
+
+  return Toggle;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Toggle, null), document.getElementById('root'));
